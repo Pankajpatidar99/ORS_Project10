f2eac30fba550a90e1d197138f8f5450e69c1d9a
Pankaj <patidarpankaj6232@gmail.com>
2023-09-24 02:02:19 +0530

    add

---
+++ ORSProject10-UI/src/app/login/forgotpassword.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { HttpServiceService } from '../http-service.service';
+import { Router } from '@angular/router';
+import { DataValidator } from '../utility/data-validator';
+
+
+@Component({
+  selector: 'app-forgot-password',
+  templateUrl: './forgotpassword.component.html'
+})
+
+export class ForgotPasswordComponent implements OnInit {
+
+  endpoint = "http://localhost:8080/Auth";
+
+  form = {
+    error: false,
+    message: "",
+    loginId: '',
+  };
+
+  inputerror = {};
+  message = '';
+
+  constructor(private httpService: HttpServiceService, private dataValidator: DataValidator, private router: Router) {
+  }
+
+
+  validate(){
+    let flag = true;
+    flag = flag && this.dataValidator.isNotNull(this.form.loginId);
+    return flag;
+  }
+
+  /**
+   * Initialize component
+   */
+  ngOnInit() {
+  }
+
+  submit() {
+    var _self = this;
+    this.httpService.get(_self.endpoint + "/fp/"+ this.form.loginId ,function (res) {
+
+      console.log('MyResponse', res);
+
+      _self.form.message = '';
+      _self.inputerror = {};
+          console.log(res.result.message+'------>msg');
+          //res.result.message = invalid loginID (when false) 
+          //res.result.message =Password has been sent to email id (when true)
+      if (res.result.message) {
+        _self.form.message = res.result.message;
+        console.log(_self.form.message+'-------> msg in sucess');
+      }
+
+
+      _self.form.error = !res.success;
+      console.log(res.success);
+      console.log(res.result.inputerror);
+//res.result.inputerror=undefined
+      if (_self.form.error && res.result.inputerror) {
+        console.log(res.result.inputerror);
+          _self.inputerror = res.result.inputerror;
+      }
+    });
+  }
+}
